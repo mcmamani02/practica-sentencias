23c07cc8c0affd1cd90b4781c7c797f668d40800
Maribel <maricelemamani@gmail.com>
2025-08-19 09:12:46 -0300

    practica con sentencia condicional if

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+cadena1=input("ingrese una palabra: ")
+cadena2=input("ingrese otra palabra: ")
+
+if cadena1.upper()==cadena2.upper():
+    print("son iguales")
+else:
+    print("son diferentes")
+    
---
+++ ejercicio2.py
@@ -0,0 +1,8 @@
+nombre=input("Ingrese su nombre: ")
+
+if nombre=="":
+    print("error tiene que ingresar un nombre")
+else:
+    print("ingreso correctamente")
+
+
