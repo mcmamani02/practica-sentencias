c0af1e0af877437c095dd6c6310f30a144babebd
Maribel <maricelemamani@gmail.com>
2025-08-25 08:41:15 -0300

    reforzando listas, funciones y operaciones

--- reforzando/02_listas_operaciones.py
+++ reforzando/02_listas_operaciones.py
@@ -1,13 +1,26 @@
 numeros = [3, 7, 2, 8, 5]
 num_mayor=0
 num_menor=numeros[0]
+suma=0
+numeros_pares=[]
 #recorrer la lista 
 for numero in numeros:
     if numero>num_mayor:
         num_mayor=numero
     if numero<num_menor:
         num_menor=numero
+    suma+=numero
+    resto=numero%2
+    if resto==0:
+        numeros_pares.append(numero)
+  
+  
+print("Lista original: ", numeros)  
+print("El numero mayor es: ",num_mayor)
+print("El numero menor es: ",num_menor)
+print("La suma de la lista es igual a: ",suma)
+print("Lista de numeros pares: ", numeros_pares)
+numeros.sort()
+print(numeros)  
 
-print(num_mayor)
-print(num_menor)
 #comparar
